76b83824623536a756b539415fb997c1efb464af
Luis <lzambrano@yaraguasoluciones.com>
2015-03-25 03:58:22 -0400

    Cambios en el render

--- app/src/main/java/com/yaraguasoluciones/anfitrion/Principal.java
+++ app/src/main/java/com/yaraguasoluciones/anfitrion/Principal.java
@@ -15,7 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 
 
@@ -45,9 +45,7 @@ public class Principal extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                //intent.setDataAndType(Uri.parse("/"), "application/pdf");
                 intent.setType("application/pdf");
-                //intent.setType("file/*.jpg");
                 startActivityForResult(intent, YOUR_RESULT_CODE);
             }
         });
@@ -60,44 +58,65 @@ public class Principal extends ActionBarActivity {
     /*
     http://stackoverflow.com/questions/3642928/adding-a-library-jar-to-an-eclipse-android-project
     http:/www.stackoverflow.com/questions/8248196/how-to-add-a-library-project-to-a-android-project
+
+    Revisar detalle en versiones de android:
+    http://hmkcode.com/android-display-selected-image-and-its-real-path/
+
+    http://stackoverflow.com/questions/10288385/android-image-picker-wrong-image
+    http://stackoverflow.com/questions/2975197/convert-file-uri-to-file-in-android
+
     */
 
         if (data!=null){
+            int currentPage = 0 ;
+
+
+
+            Uri selectedImageUri = Uri.parse(data.getDataString());
+
+//            File file = new File(selectedImageUri.toString());
+            File file = new File("/sdcard/Download/22_Menen.pdf");
 
             PdfRenderer renderer = null;
             try {
+
                 /*
                 https://developer.android.com/reference/android/graphics/pdf/PdfRenderer.html
                 http://stackoverflow.com/questions/6715898/what-is-parcelfiledescriptor-in-android
+                https://github.com/digipost/Android-Pdf
                  */
 
-
-                ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(Uri.parse(data.getDataString()),"r");
-                renderer = new PdfRenderer(parcelFileDescriptor);
-                final int pageCount = renderer.getPageCount();
-                Bitmap mBitmap = null;
-
-                for (int i = 0; i < pageCount; i++) {
-                    PdfRenderer.Page page = renderer.openPage(i);
-
-                    // say we render for showing on the screen
-                    page.render(mBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
-
-                    // do stuff with the bitmap
-
-                    // close the page
-                    page.close();
-                }
-
-                // close the renderer
-                renderer.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+                renderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
 
+            if(currentPage > renderer.getPageCount()){
+                currentPage = renderer.getPageCount()-1;
+            }
+            //renderer.openPage(currentPage).render(bitmap, new Rect(0, 0, 500, 500), new Matrix(), PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+
+
+            final int pageCount = renderer.getPageCount();
+            Bitmap mBitmap = null;
+
+            for (int i = 0; i < pageCount; i++) {
+                PdfRenderer.Page page = renderer.openPage(i);
+
+                // say we render for showing on the screen
+                //imageView = (ImageView) findViewById(R.id.imageView);
+                Bitmap bitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_4444);
+                page.render(mBitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+
+                // do stuff with the bitmap
+
+                // close the page
+                page.close();
+            }
+
+            // close the renderer
+            renderer.close();
 
 
 
